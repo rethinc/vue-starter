b7b40644c36666b804b6b0fb16482401273377c5
Chris Weber <chris.weber@rethinc.ch>
2022-03-05 14:29:00 +0100

    Adds install starter vue application

--- build-environment.sh
+++ build-environment.sh
@@ -13,10 +13,12 @@ main() {
     exit 1
   fi
   local build_dir="${dir}/build"
+  local starter_dir="$(dirname -- "${build_dir}")"
   rm -rf "${build_dir}"
 
   createNewVueProject "${build_dir}" "${project_name}"
   installStarterDependencies "${build_dir}"
+  intallStarterVueApplication "${build_dir}" "${starter_dir}"
 }
 
 createNewVueProject() {
@@ -25,41 +27,59 @@ createNewVueProject() {
 
   mkdir -p "${build_dir}"
   pushd "${build_dir}"
-    npm init vite@latest "${project_name}" -- --template vue-ts
-    rm -r "${build_dir}/${project_name}"/.vscode
-    mv "${build_dir}/${project_name}"/{*,.[^.]*} "${build_dir}"
-    rm -r "${project_name}"
-    npm install
+  npm init vite@latest "${project_name}" -- --template vue-ts
+  rm -r "${build_dir}/${project_name}"/.vscode
+  mv "${build_dir}/${project_name}"/{*,.[^.]*} "${build_dir}"
+  rm -r "${project_name}"
+  npm install
   popd
 }
 
 installStarterDependencies() {
-    local build_dir="${1}"
+  local build_dir="${1}"
 
-    pushd "${build_dir}"
-      npm install --save-dev \
-      eslint \
-      @typescript-eslint/parser \
-      @typescript-eslint/eslint-plugin \
-      eslint-plugin-vue \
-      vue-eslint-parser \
-      prettier \
-      eslint-config-prettier \
-      eslint-plugin-prettier \
-      jest \
-      ts-jest \
-      @types/jest \
-      sass \
-      ts-node \
-      npm-check-updates \
-      svgo
+  pushd "${build_dir}"
+  npm install --save-dev \
+    eslint \
+    @typescript-eslint/parser \
+    @typescript-eslint/eslint-plugin \
+    eslint-plugin-vue \
+    vue-eslint-parser \
+    prettier \
+    eslint-config-prettier \
+    eslint-plugin-prettier \
+    jest \
+    ts-jest \
+    @types/jest \
+    sass \
+    ts-node \
+    npm-check-updates \
+    svgo
 
-      npm install --save \
-      vue-router
+  npm install --save \
+    vue-router
 
-      npx npm-check-updates -u
-      npm update
-    popd
+  npx npm-check-updates -u
+  npm update
+  popd
+}
+
+intallStarterVueApplication() {
+  local build_dir="${1}"
+  local starter_dir="${2}"
+
+  pushd "${starter_dir}"
+  rm -r "${build_dir}/src"
+  cp -r src "${build_dir}/"
+  cp .eslintrc.json \
+    .nvmrc \
+    .prettierrc.json \
+    index.html \
+    jest.config.json \
+    tsconfig.json \
+    vite.config.ts \
+    "${build_dir}/"
+  popd
 }
 
 main "$@"
