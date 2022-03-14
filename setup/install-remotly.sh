e3944ae50d242d633a4984820b756bf0b8ebfc0d
Yves Bonjour <yves.bonjour@rethinc.ch>
2022-03-14 16:28:32 +0100

    Refactors setup scripts

---
+++ setup/install-remotly.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# This script is used as a remote install script.
+# Therfore it needs to be self contained and can not depend
+# on any dependencies (e.g. files in includes).
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+main() {
+  git=$(check_tool git)
+    
+
+  local product_name="${1:-}"
+  if [ -z "${product_name}" ]; then
+    echo "USAGE: $0 <product name>"
+    exit 1
+  fi
+
+  local product_folder="${dir}/${product_name}"
+
+  local download_folder=$(mktemp -d)
+  trap "rm -rf \"${download_folder}\"" EXIT
+  download "https://github.com/rethinc/vue-starter.git" "${download_folder}"
+
+  "${download_folder}/setup/install.sh" "${download_folder}/vue-application" "${product_folder}" 
+}
+
+download() {
+  local from="${1}"
+  local to="${2}"
+  "${git}" clone --depth 1 "${from}" "${to}"
+  rm -rf "${to}/.git"
+}
+
+check_tool() {
+  local name="${1}"
+  local binary=$(which ${name} || true)
+
+  if [ -z "${binary}" ]; then
+    (>&2 echo "ERROR: ${name} not found.")
+    (>&2 echo "This script depends on ${name}. Please install it first before executing this scirpt.")
+    exit 127
+  fi
+  echo "${binary}"
+}
+
+main "$@"
---
+++ setup/install.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+dir=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+
+source "${dir}/includes/check-tool.bash.inc"
+sed=$(check_tool sed)
+npm=$(check_tool npm)
+npx=$(check_tool npx)
+
+
+main() {
+  local source_folder="${1:-}"
+  if [ -z "${source_folder}" ]; then
+	echo "USAGE: $0 <source folder> <destination folder> <product name>"
+	exit 1
+  fi
+
+  local destination_folder="${2:-}"
+  if [ -z "${destination_folder}" ]; then
+	echo "USAGE: $0 <source folder> <destination folder> <product name>"
+	exit 1
+  fi
+
+
+  local product_name="${3:-}"
+  if [ -z "${product_name}" ]; then
+	echo "USAGE: $0 <source folder> <destination folder> <product name>"
+	exit 1
+  fi
+
+  local product_folder="${destination_folder}/${product_name}"
+  if [ -e "${product_folder}" ]; then
+  	(>&2 echo "Folder ${product_folder} already exists")
+  	exit 1
+  fi
+
+
+  mkdir -p "${product_folder}"
+  cp -r "${source_folder}"/{*,.[^.]*} "${product_folder}/"
+  "${sed}" -i -e "s/{{productName}}/${product_name}/g" "${product_folder}/README.md"
+  "${sed}" -i -e "s/{{productName}}/${product_name}/g" "${product_folder}/package.json"
+  install_dependencies "${product_folder}"
+}
+
+install_dependencies() {
+  local product_folder="${1}"
+
+  pushd "${product_folder}"
+    "${npm}" install
+    "${npx}" npm-check-updates -u
+    "${npm}" update
+  popd
+}
+
+main "$@"
