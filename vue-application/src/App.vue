78f10b8d15b3498336af8ffde624d485ba346a9e
Chris Weber <chris.weber@rethinc.ch>
2022-03-25 10:09:13 +0100

    Simplified App.vue

--- vue-application/src/App.vue
+++ vue-application/src/App.vue
@@ -2,16 +2,6 @@
   <RouterView />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-export default defineComponent({
-  name: 'App',
-  setup() {
-    return {}
-  },
-})
-</script>
-
 <style lang="scss">
 @import 'assets/styles/global.scss';
 </style>
