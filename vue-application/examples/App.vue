d963aaa042899133f78123cac457d53fc6bbed50
Chris Weber <chris.weber@rethinc.ch>
2024-03-28 20:57:07 +0100

    Move navigation to top most App component

--- vue-application/examples/App.vue
+++ vue-application/examples/App.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+import NavigationView from './application/overview/NavigationView.vue'
+</script>
+
 <template>
+  <NavigationView class="navigation" />
   <RouterView />
 </template>
 
--- vue-application/examples/application/overview/ExamplesOverview.vue
+++ vue-application/examples/application/overview/ExamplesOverview.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-import NavigationView from './NavigationView.vue'
 import ViewExampleIFrame from './ViewExampleIFrame.vue'
 </script>
-
 <template>
   <div>
-    <NavigationView class="navigation" />
     <ViewExampleIFrame />
   </div>
 </template>
