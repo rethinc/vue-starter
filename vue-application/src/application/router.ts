f5894e329506fb82181aa09f7fc162e7e9a84fd3
Chris Weber <chris.weber@rethinc.ch>
2024-03-29 13:19:59 +0100

    Change folder entryPoint to use kebab case convention entry-point

--- vue-application/src/application/router.ts
+++ vue-application/src/application/router.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import EntryPoint from '@/application/entryPoint/EntryPoint.vue'
+import EntryPoint from '@/application/entry-point/EntryPoint.vue'
 
 export const router = createRouter({
   history: createWebHistory(),
