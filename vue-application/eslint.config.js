09eda443816e40c2dc29e7b659d11898912d453e
Chris Weber <chris.weber@rethinc.ch>
2024-04-29 11:51:13 +0200

    Add prettier for code formatting

--- vue-application/eslint.config.js
+++ vue-application/eslint.config.js
@@ -1,31 +1,34 @@
-import eslintJs from "@eslint/js";
-import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
-import eslintPluginVue from "eslint-plugin-vue";
-import typescriptEslint from "typescript-eslint";
+import eslintJs from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import eslintPluginVue from 'eslint-plugin-vue'
+import typescriptEslint from 'typescript-eslint'
 
 export default typescriptEslint.config(
-    eslintJs.configs.recommended,
-    ...typescriptEslint.configs.recommended,
-    ...eslintPluginVue.configs['flat/recommended'],
-    {
-        plugins: {
-            'typescript-eslint': typescriptEslint.plugin,
-            'simple-import-sort': eslintPluginSimpleImportSort
-        },
-        rules: {
-            "simple-import-sort/imports": "error",
-            "simple-import-sort/exports": "error",
-        },
-        languageOptions: {
-            parserOptions: {
-                parser: typescriptEslint.parser,
-                project: './tsconfig.json',
-                extraFileExtensions: ['.vue'],
-                sourceType: 'module',
-            },
-        },
+  eslintJs.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  ...eslintPluginVue.configs['flat/recommended'],
+
+  {
+    plugins: {
+      'typescript-eslint': typescriptEslint.plugin,
+      'simple-import-sort': eslintPluginSimpleImportSort,
     },
-    {
-        ignores:['dist/*']
-    }
-);
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+    languageOptions: {
+      parserOptions: {
+        parser: typescriptEslint.parser,
+        project: './tsconfig.json',
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module',
+      },
+    },
+  },
+  {
+    ignores: ['dist/*'],
+  },
+  eslintPluginPrettierRecommended
+)
