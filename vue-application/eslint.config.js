f7d8a014190d7d92ce2cf8ad477aa0c60ae93665
Chris Weber <chris.weber@rethinc.ch>
2024-04-29 11:43:42 +0200

    Add linting import order

--- vue-application/eslint.config.js
+++ vue-application/eslint.config.js
@@ -1,6 +1,7 @@
 import eslintJs from "@eslint/js";
-import typescriptEslint from "typescript-eslint";
+import eslintPluginSimpleImportSort from "eslint-plugin-simple-import-sort";
 import eslintPluginVue from "eslint-plugin-vue";
+import typescriptEslint from "typescript-eslint";
 
 export default typescriptEslint.config(
     eslintJs.configs.recommended,
@@ -9,6 +10,11 @@ export default typescriptEslint.config(
     {
         plugins: {
             'typescript-eslint': typescriptEslint.plugin,
+            'simple-import-sort': eslintPluginSimpleImportSort
+        },
+        rules: {
+            "simple-import-sort/imports": "error",
+            "simple-import-sort/exports": "error",
         },
         languageOptions: {
             parserOptions: {
